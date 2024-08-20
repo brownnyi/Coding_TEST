c976241deb8c83da93cf4e43c17571b137f66c3a
brownnyi <brown970523@naver.com>
2024-08-20 15:47:41 +0900

    [Bronze V] Title: 사칙연산, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.py
@@ -0,0 +1,11 @@
+def cal(a, b):
+    print(int(a) + int(b))
+    print(int(a) - int(b))
+    print(int(a) * int(b))
+    print(int(a) // int(b))
+    print(int(a) % int(b))
+
+a, b = input().split()
+
+
+cal(a, b)
