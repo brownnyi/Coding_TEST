3ffe63194d669d597a7ea5f5a05defcb99ea4453
brownnyi <brown970523@naver.com>
2024-10-09 17:29:52 +0900

    Create running_total.sql

---
+++ SQL/datarian/running_total.sql
@@ -0,0 +1,15 @@
+SELECT
+  m1.measured_at,
+  pm10,
+  (
+    SELECT
+      SUM(m2.pm10)
+    FROM
+      measurements as m2
+    WHERE
+      m2.measured_at BETWEEN '2022-01-01' AND m1.measured_at
+  ) as pm10_running_total
+FROM
+  measurements as m1
+ORDER BY
+  measured_at
