1fb3b4a389e0482a7095e08f4925e0d19178cfea
brownnyi <brown970523@naver.com>
2024-10-16 12:41:11 +0900

    Update WITH.sql

--- SQL/datarian/WITH.sql
+++ SQL/datarian/WITH.sql
@@ -2,16 +2,16 @@ WITH
   total_revenue as (
     SELECT
       day,
-      SUM(total_bill) as sales
+      SUM(bill) as sales
     FROM
-      tips
+      AB
     GROUP BY
       day
   )
 SELECT
-  CONCAT(ROUND(A.total_bill * 100.0 / B.sales, 2), '%') as total_per
+  CONCAT(ROUND(A.bill * 100.0 / B.sales, 2), '%') as total_per
 FROM
-  tips as A
+  AB as A
   JOIN total_revenue as B ON A.day = B.day
 ORDER BY
   1 DESC;
